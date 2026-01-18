7eb099ca398ca54762f18c8c1c940c9670a5ee58
krishbhai9000-design <krishbhai9000@gmail.com>
2026-01-19 00:26:23 +0530

    main.py

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ app = Flask(__name__)
 def home():
     return "API running", 200
 
+
+@app.route("/configvalue", methods=["GET", "POST"])
+def configvalue():
+    return Response(
+        """-----BEGIN CERTIFICATE-----
+TEST_CERTIFICATE_DATA
+-----END CERTIFICATE-----""",
+        mimetype="text/plain"
+    )
+
+
 @app.route("/get_proxy", methods=["GET", "POST"])
 def get_proxy():
-    proxy = "127.0.0.1:8080"
-    return Response(proxy, mimetype="text/plain")
+    return "127.0.0.1:8080", 200
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
